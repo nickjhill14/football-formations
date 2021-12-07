5e76c729d6bed8f485ba7aed51979d520fb7ea6f
nicholasjhill <nick.hill@thoughtworks.com>
2021-12-07 17:58:46 +0000

    Add formation select functionality

--- src/components/FootballFormations.test.tsx
+++ src/components/FootballFormations.test.tsx
@@ -1,29 +1,55 @@
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { axe, toHaveNoViolations } from 'jest-axe';
+import { axe } from 'jest-axe';
 import {
 	FOOTBALL_FORMATIONS_HEADING,
 	FOOTBALL_FORMATIONS_SUBHEADING,
-	FORMATIONS_SECTION_LABEL,
+	FORMATION_DISPLAY_SECTION_LABEL,
+	FORMATIONS,
+	FORMATION_SELECTION_SECTION_LABEL,
 	FORMATIONS_SELECT_LABEL,
 	HEADER_LABEL,
 	MAIN_LABEL,
+	NO_FORMATION,
 } from '../constants/footballFormations';
 import FootballFormations from './FootballFormations';
 
-expect.extend(toHaveNoViolations);
-
 describe('<FootballFormations/>', () => {
+	const { selectOptions, tab } = userEvent;
+
 	describe('rendering', () => {
 		it('should render the initial page', () => {
-			const { getByRole } = render(<FootballFormations />);
+			const { getByRole, queryByRole } = render(<FootballFormations />);
 
 			expect(getByRole('main', { name: MAIN_LABEL })).toBeInTheDocument();
 			expect(getByRole('banner', { name: HEADER_LABEL })).toBeInTheDocument();
 			expect(getByRole('heading', { name: FOOTBALL_FORMATIONS_HEADING })).toBeInTheDocument();
 			expect(getByRole('heading', { name: FOOTBALL_FORMATIONS_SUBHEADING })).toBeInTheDocument();
-			expect(getByRole('region', { name: FORMATIONS_SECTION_LABEL })).toBeInTheDocument();
+			expect(getByRole('region', { name: FORMATION_SELECTION_SECTION_LABEL })).toBeInTheDocument();
 			expect(getByRole('combobox', { name: FORMATIONS_SELECT_LABEL })).toBeInTheDocument();
+			expect(getByRole('option', { name: NO_FORMATION })).toBeInTheDocument();
+			FORMATIONS.forEach((formation) => expect(getByRole('option', { name: formation })).toBeInTheDocument());
+			expect(queryByRole('region', { name: FORMATION_DISPLAY_SECTION_LABEL })).not.toBeInTheDocument();
+		});
+	});
+
+	describe('interaction', () => {
+		it('should show selected formation on selection', () => {
+			const selectedFormation = FORMATIONS[0];
+
+			const { getByRole } = render(<FootballFormations />);
+			selectOptions(getByRole('combobox', { name: FORMATIONS_SELECT_LABEL }), selectedFormation);
+
+			expect(getByRole('region', { name: FORMATION_DISPLAY_SECTION_LABEL })).toBeInTheDocument();
+			expect(getByRole('heading', { name: selectedFormation })).toBeInTheDocument();
+		});
+
+		it('should remove selected formation on change back to none', () => {
+			const { getByRole, queryByRole } = render(<FootballFormations />);
+			selectOptions(getByRole('combobox', { name: FORMATIONS_SELECT_LABEL }), FORMATIONS[0]);
+			selectOptions(getByRole('combobox', { name: FORMATIONS_SELECT_LABEL }), NO_FORMATION);
+
+			expect(queryByRole('region', { name: FORMATION_DISPLAY_SECTION_LABEL })).not.toBeInTheDocument();
 		});
 	});
 
@@ -36,7 +62,7 @@ describe('<FootballFormations/>', () => {
 
 		it('should focus the formation select after initial tab interaction', () => {
 			const { getByRole } = render(<FootballFormations />);
-			userEvent.tab();
+			tab();
 
 			expect(getByRole('combobox', { name: FORMATIONS_SELECT_LABEL })).toHaveFocus();
 		});
--- src/components/FootballFormations.tsx
+++ src/components/FootballFormations.tsx
@@ -1,23 +1,45 @@
+import { ChangeEvent, useState } from 'react';
 import {
 	FOOTBALL_FORMATIONS_HEADING,
 	FOOTBALL_FORMATIONS_SUBHEADING,
-	FORMATIONS_SECTION_LABEL,
+	FORMATION_DISPLAY_SECTION_LABEL,
+	FORMATIONS,
+	FORMATION_SELECTION_SECTION_LABEL,
 	FORMATIONS_SELECT_LABEL,
 	HEADER_LABEL,
 	MAIN_LABEL,
+	NO_FORMATION,
 } from '../constants/footballFormations';
 
 export default function FootballFormations() {
+	const [selectedFormation, setSelectedFormation] = useState('');
+
+	const handleFormationSelect = (event: ChangeEvent<HTMLSelectElement>): void => {
+		setSelectedFormation(event.currentTarget.value);
+	};
+
 	return (
 		<main aria-label={MAIN_LABEL}>
 			<header aria-label={HEADER_LABEL}>
 				<h1>{FOOTBALL_FORMATIONS_HEADING}</h1>
 				<h2>{FOOTBALL_FORMATIONS_SUBHEADING}</h2>
 			</header>
-			<section aria-label={FORMATIONS_SECTION_LABEL}>
+			<section aria-label={FORMATION_SELECTION_SECTION_LABEL}>
 				<label htmlFor={FORMATIONS_SELECT_LABEL}>{FORMATIONS_SELECT_LABEL}</label>
-				<select id={FORMATIONS_SELECT_LABEL} />
+				<select id={FORMATIONS_SELECT_LABEL} onChange={handleFormationSelect}>
+					<option defaultChecked>{NO_FORMATION}</option>
+					{FORMATIONS.map((formation) => (
+						<option key={formation} value={formation}>
+							{formation}
+						</option>
+					))}
+				</select>
 			</section>
+			{selectedFormation && selectedFormation !== NO_FORMATION && (
+				<section aria-label={FORMATION_DISPLAY_SECTION_LABEL}>
+					<h3>{selectedFormation}</h3>
+				</section>
+			)}
 		</main>
 	);
 }
